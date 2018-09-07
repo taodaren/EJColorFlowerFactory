8fa225d235b1f6858fce565638df692d63befaf3
taodaren <397771543@qq.com>
2018-09-07 16:05:31 +0800

    add debug

--- app/src/main/java/com/box_tech/fireworksmachine/MainActivity.java
+++ app/src/main/java/com/box_tech/fireworksmachine/MainActivity.java
@@ -44,7 +44,7 @@ import static android.app.AlertDialog.THEME_HOLO_DARK;
 
 public class MainActivity extends BLEManagerActivity implements ISendCommand,
         DeviceConfigFragment.OnFragmentInteractionListener,
-        GroupFragment.OnFragmentInteractionListener{
+        GroupFragment.OnFragmentInteractionListener {
     private final static String TAG = "MainActivity";
     private final List<Device> mDeviceList = new LinkedList<>();
     private final Map<String, ProtocolWithDevice> mProtocolList = new ArrayMap<>();
@@ -53,8 +53,8 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     @SuppressWarnings("SpellCheckingInspection")
     private final static ParcelUuid UUID_GATT_SERVICE
             = ParcelUuid.fromString("6e400001-b5a3-f393-e0a9-e50e24dcca9e");
-    //private final static UUID UUID_GATT_CHARACTERISTIC_NOTIFY
-    //        = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
+    private final static UUID UUID_GATT_CHARACTERISTIC_NOTIFY
+            = UUID.fromString("6e400003-b5a3-f393-e0a9-e50e24dcca9e");
     @SuppressWarnings("SpellCheckingInspection")
     private final static UUID UUID_GATT_CHARACTERISTIC_WRITE
             = UUID.fromString("6e400002-b5a3-f393-e0a9-e50e24dcca9e");
@@ -70,10 +70,9 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);// 保持常亮的屏幕的状态
 
-        mDeviceListAdapter = new DeviceListAdapter(this, mDeviceList );
+        mDeviceListAdapter = new DeviceListAdapter(this, mDeviceList);
         mDeviceListAdapter.setSendCommand(this);
 
         LoginSession session = Settings.getLoginSessionInfo(this);
@@ -92,7 +91,7 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
 
     @Override
     protected void onDestroy() {
-        if(mSelectDialog!=null){
+        if (mSelectDialog != null) {
             mSelectDialog.dismiss();
             mSelectDialog = null;
         }
@@ -106,8 +105,8 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
             R.id.category_my
     };
 
-    private void setupCategoryClick(){
-        for(int i=0;i<category_id.length;i++){
+    private void setupCategoryClick() {
+        for (int i = 0; i < category_id.length; i++) {
             TextView tv = findViewById(category_id[i]);
             final int position = i;
             tv.setOnClickListener(new View.OnClickListener() {
@@ -123,9 +122,9 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     private final ViewPager.OnPageChangeListener mPageChangeListener = new ViewPager.SimpleOnPageChangeListener() {
         @Override
         public void onPageSelected(int position) {
-            for(int i=0;i<category_id.length;i++){
+            for (int i = 0; i < category_id.length; i++) {
                 TextView tv = findViewById(category_id[i]);
-                tv.setSelected(i==position);
+                tv.setSelected(i == position);
             }
         }
     };
@@ -134,7 +133,7 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     @Override
     public void scanDevice() {
         refresh();
-        if(mDeviceList.isEmpty()){
+        if (mDeviceList.isEmpty()) {
             showDeviceAddressSelectDialog();
         }
     }
@@ -155,11 +154,11 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
 
     private Dialog mSelectDialog;
 
-    private void showDeviceAddressSelectDialog(){
+    private void showDeviceAddressSelectDialog() {
         final View view = View.inflate(this, R.layout.device_list_dialog, null);
         final ListView lv = view.findViewById(R.id.lv_device_list);
         mFoundDeviceAddressListAdapter = new ArrayAdapter<String>(this,
-                R.layout.device_address_item, R.id.tv_device_address, mFoundDeviceAddressList){
+                R.layout.device_address_item, R.id.tv_device_address, mFoundDeviceAddressList) {
             @NonNull
             @Override
             public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -168,7 +167,7 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
                 t.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        CheckedTextView t = (CheckedTextView)v;
+                        CheckedTextView t = (CheckedTextView) v;
                         t.toggle();
                         lv.setItemChecked(position, t.isChecked());
                     }
@@ -187,13 +186,12 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         SparseBooleanArray selected_array = lv.getCheckedItemPositions();
-                        for(int i = 0; i< mFoundDeviceAddressList.size(); i++){
-                            if(selected_array.get(i)){
-                                Log.i(TAG, "select "+mFoundDeviceAddressList.get(i));
+                        for (int i = 0; i < mFoundDeviceAddressList.size(); i++) {
+                            if (selected_array.get(i)) {
+                                Log.i(TAG, "select " + mFoundDeviceAddressList.get(i));
                                 add_device(mFoundDeviceAddressList.get(i), 0);
-                            }
-                            else{
-                                Log.i(TAG, "not select "+mFoundDeviceAddressList.get(i));
+                            } else {
+                                Log.i(TAG, "not select " + mFoundDeviceAddressList.get(i));
                             }
                         }
                         mFoundDeviceAddressListAdapter = null;
@@ -212,16 +210,17 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     }
 
     @Override
-    protected void onStopScan(){
-        if(mFoundDeviceProgressBar!=null){
+    protected void onStopScan() {
+        if (mFoundDeviceProgressBar != null) {
             mFoundDeviceProgressBar.setVisibility(View.GONE);
             mFoundDeviceProgressBar = null;
         }
     }
 
-    private class ProtocolWithDevice extends Protocol{
+    private class ProtocolWithDevice extends Protocol {
         final Device device;
-        ProtocolWithDevice(@NonNull Device device){
+
+        ProtocolWithDevice(@NonNull Device device) {
             this.device = device;
         }
 
@@ -237,7 +236,7 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
         }
 
         @Override
-        protected void onReceivePackage(@NonNull DeviceConfig config){
+        protected void onReceivePackage(@NonNull DeviceConfig config) {
             device.setConfig(config);
             runOnUiThread(new Runnable() {
                 @Override
@@ -248,7 +247,7 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
         }
 
         @Override
-        protected void onReceivePackage( @NonNull final byte[] pkg, int pkg_len) {
+        protected void onReceivePackage(@NonNull final byte[] pkg, int pkg_len) {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
@@ -258,9 +257,9 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
         }
     }
 
-    private static boolean contain(GetDeviceList.Result.DeviceInformation[] list, String mac){
-        for(GetDeviceList.Result.DeviceInformation d : list){
-            if(d.getMac().equals(mac)){
+    private static boolean contain(GetDeviceList.Result.DeviceInformation[] list, String mac) {
+        for (GetDeviceList.Result.DeviceInformation d : list) {
+            if (d.getMac().equals(mac)) {
                 return true;
             }
         }
@@ -271,26 +270,26 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     public void set_registered_device(GetDeviceList.Result.DeviceInformation[] list) {
         List<String> rm = new LinkedList<>();
         List<Device> rmd = new LinkedList<>();
-        for(String mac : mProtocolList.keySet()){
-            if(!contain(list, mac)){
+        for (String mac : mProtocolList.keySet()) {
+            if (!contain(list, mac)) {
                 rm.add(mac);
                 rmd.add(getDevice(mac));
             }
         }
-        for(String mac : rm){
+        for (String mac : rm) {
             mProtocolList.remove(mac);
         }
-        for(Device d : rmd){
+        for (Device d : rmd) {
             mDeviceList.remove(d);
         }
 
-        for(GetDeviceList.Result.DeviceInformation d : list){
+        for (GetDeviceList.Result.DeviceInformation d : list) {
             add_device(d.getMac(), d.getId());
         }
     }
 
-    private void add_device(final String mac, long id){
-        if( !mProtocolList.containsKey(mac) ){
+    private void add_device(final String mac, long id) {
+        if (!mProtocolList.containsKey(mac)) {
             final Device d = new Device(mac);
             d.setId(id);
             mDeviceList.add(d);
@@ -301,14 +300,14 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     }
 
     @Override
-    protected void onFoundDevice(final BluetoothDevice device, @Nullable List<ParcelUuid> serviceUuids){
+    protected void onFoundDevice(final BluetoothDevice device, @Nullable List<ParcelUuid> serviceUuids) {
         String name = device.getName();
         String mac = device.getAddress();
-        if( name.indexOf("EEJING-CHJ")!=0){
+        if (name.indexOf("EEJING-CHJ") != 0) {
             return;
         }
-        if(mFoundDeviceAddressListAdapter != null){
-            if( ! mFoundDeviceAddressList.contains(mac)){
+        if (mFoundDeviceAddressListAdapter != null) {
+            if (!mFoundDeviceAddressList.contains(mac)) {
                 mFoundDeviceAddressList.add(mac);
                 mFoundDeviceAddressListAdapter.notifyDataSetChanged();
             }
@@ -317,9 +316,9 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     }
 
     //private boolean getStatusFlag = true;
-    private Device getDevice(String mac){
-        for(Device device : mDeviceList){
-            if(device.getAddress().equals(mac)){
+    private Device getDevice(String mac) {
+        for (Device device : mDeviceList) {
+            if (device.getAddress().equals(mac)) {
                 return device;
             }
         }
@@ -329,32 +328,31 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     private boolean mRequestConfig = false;
 
     @Override
-    protected void onDeviceReady(final String mac){
-        if( setSendDefaultChannel(mac, UUID_GATT_CHARACTERISTIC_WRITE ) ){
+    protected void onDeviceReady(final String mac) {
+        if (setSendDefaultChannel(mac, UUID_GATT_CHARACTERISTIC_WRITE)) {
             Device device = getDevice(mac);
-            if(device!=null){
+            if (device != null) {
                 device.setConnected(true);
                 mDeviceListAdapter.notifyDataSetChanged();
             }
-
             registerRefreshStatus(mac);
         }
     }
 
-    private void registerRefreshStatus(final String mac){
+    private void registerRefreshStatus(final String mac) {
         registerPeriod(mac + "-status", new Runnable() {
                     @Override
                     public void run() {
                         ProtocolWithDevice pd = mProtocolList.get(mac);
-                        if(pd==null){
+                        if (pd == null) {
                             return;
                         }
                         Device device = getDevice(mac);
-                        if(device!=null){
+                        if (device != null) {
                             DeviceConfig config = device.getConfig();
-                            long id = (config==null)?0:config.mID;
-                            if(config==null || mRequestConfig){
-                                mRequestConfig = ! send(mac, Protocol.get_config_package(id), true);
+                            long id = (config == null) ? 0 : config.mID;
+                            if (config == null || mRequestConfig) {
+                                mRequestConfig = !send(mac, Protocol.get_config_package(id), true);
                             }
                             send(mac, Protocol.get_status_package(id), true);
                         }
@@ -364,36 +362,35 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     }
 
     @Override
-    protected void onDeviceConnect(String mac){
-        Log.i(TAG, "onDeviceConnect "+mac);
+    protected void onDeviceConnect(String mac) {
+        Log.i(TAG, "onDeviceConnect " + mac);
         registerRefreshStatus(mac);
 
         Device device = getDevice(mac);
-        if(device!=null){
+        if (device != null) {
             device.setConnected(true);
             mDeviceListAdapter.notifyDataSetChanged();
-        }
-        else{
+        } else {
             Log.i(TAG, "onDeviceConnect no device");
         }
     }
 
     @Override
-    protected void onDeviceDisconnect(String mac){
+    protected void onDeviceDisconnect(String mac) {
         unregisterPeriod(mac + "-status");
 
         Device device = getDevice(mac);
-        if(device!=null){
+        if (device != null) {
             device.setConnected(false);
             mDeviceListAdapter.notifyDataSetChanged();
         }
     }
 
     @Override
-    protected void onReceive(String mac, byte[] data){
+    protected void onReceive(String mac, byte[] data) {
         super.onReceive(mac, data);
         Protocol p = mProtocolList.get(mac);
-        if(p!=null){
+        if (p != null) {
             p.onReceive(data);
         }
     }
@@ -402,7 +399,7 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
     public void sendCommand(@NonNull Device device, @NonNull byte[] pkg) {
         String mac = device.getAddress();
         Protocol p = mProtocolList.get(mac);
-        if(p!=null){
+        if (p != null) {
             send(device.getAddress(), pkg, true);
             mRequestConfig = true;
         }
@@ -414,31 +411,31 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
         sendCommand(device, pkg);
     }
 
-    private static class PackageNeedAck{
+    private static class PackageNeedAck {
         final byte[] cmd_pkg;
         final String mac;
         final long send_time;
         final OnReceivePackage callback;
 
-        PackageNeedAck(String mac, byte[] cmd_pkg, OnReceivePackage callback){
+        PackageNeedAck(String mac, byte[] cmd_pkg, OnReceivePackage callback) {
             this.mac = mac;
             this.cmd_pkg = cmd_pkg;
             this.callback = callback;
             this.send_time = System.currentTimeMillis();
         }
 
-        boolean isTimeout(){
-            return (System.currentTimeMillis()-send_time)>ACK_TIMEOUT;
+        boolean isTimeout() {
+            return (System.currentTimeMillis() - send_time) > ACK_TIMEOUT;
         }
     }
 
     private final LinkedList<PackageNeedAck> mPackageNeedAckList = new LinkedList<>();
 
-    private void doMatch(String mac, byte[] ack_pkg){
-        Log.i(TAG, "doMatch "+ Util.hex(ack_pkg, ack_pkg.length) + " from " + mPackageNeedAckList.size());
-        for(PackageNeedAck p : mPackageNeedAckList){
-            Log.i(TAG, "doMatch check "+ Util.hex(p.cmd_pkg, p.cmd_pkg.length));
-            if( p.mac.equals(mac) && Protocol.isMatch(p.cmd_pkg, ack_pkg) ){
+    private void doMatch(String mac, byte[] ack_pkg) {
+        Log.i(TAG, "doMatch " + Util.hex(ack_pkg, ack_pkg.length) + " from " + mPackageNeedAckList.size());
+        for (PackageNeedAck p : mPackageNeedAckList) {
+            Log.i(TAG, "doMatch check " + Util.hex(p.cmd_pkg, p.cmd_pkg.length));
+            if (p.mac.equals(mac) && Protocol.isMatch(p.cmd_pkg, ack_pkg)) {
                 Log.i(TAG, "doMatch match");
                 mPackageNeedAckList.remove(p);
                 p.callback.ack(ack_pkg);
@@ -447,17 +444,15 @@ public class MainActivity extends BLEManagerActivity implements ISendCommand,
         }
     }
 
-    private void doTimeoutCheck(){
-        for(PackageNeedAck p : mPackageNeedAckList){
-            if( p.isTimeout() ){
+    private void doTimeoutCheck() {
+        for (PackageNeedAck p : mPackageNeedAckList) {
+            if (p.isTimeout()) {
                 mPackageNeedAckList.remove(p);
                 p.callback.timeout();
                 return;
             }
         }
     }
-
-
 
     @Override
     protected void poll() {
